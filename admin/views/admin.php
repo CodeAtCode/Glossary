0303b3f000f99db0a5c8aadd7ddaf58ed02c9613
eugenio <eugenio@fedora.codeat>
2016-02-22 15:31:22 +0100

    es_ES and pt_PT localizations

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -156,10 +156,9 @@
         <div class="postbox codeat social">
             <h3 class="hndle"><span><?php _e( 'Follow us', $this->plugin_slug ); ?></span></h3>
             <div class="inside">
-                <a href="https://facebook.com/codeatco/" target="_blank"><img src="http://i2.wp.com/codeat.co/wp-content/uploads/2016/02/social-facebook-outline.png?w=52" alt="facebook"></a>
-                <a href="https://twitter.com/codeatco/" target="_blank"><img src="http://i0.wp.com/codeat.co/wp-content/uploads/2016/02/social-twitter-outline.png?w=52" alt="twitter"></a>
-                <a href="https://linkedin.com/company/codeat/" target="_blank"><img src="http://i1.wp.com/codeat.co/wp-content/uploads/2016/02/social-linkedin-outline.png?w=52" alt="linkedin"></a>
-                <a href="https://github.com/CodeAtCode/" target="_blank"><img src="http://i1.wp.com/codeat.co/wp-content/uploads/2016/02/social-github-outline.png?w=52" alt="github"></a>
+                <a href="https://facebook.com/codeatco/" target="_blank"><img src="http://i2.wp.com/codeat.co/wp-content/uploads/2016/02/social-facebook.png?w=52" alt="facebook"></a>
+                <a href="https://twitter.com/codeatco/" target="_blank"><img src="http://i0.wp.com/codeat.co/wp-content/uploads/2016/02/social-twitter.png?w=52" alt="twitter"></a>
+                <a href="https://linkedin.com/company/codeat/" target="_blank"><img src="http://i1.wp.com/codeat.co/wp-content/uploads/2016/02/social-linkedin.png?w=52" alt="linkedin"></a>
             </div>
         </div>
     </div>
